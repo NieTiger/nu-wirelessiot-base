9aa7b9b16ed187329066f27f98bcae0238b13264
Tiger Nie <nhl0819@gmail.com>
2021-02-07 10:45:19 +0800

    environmental sensing service

--- software/apps/ble_ess_example/main.c
+++ software/apps/ble_ess_example/main.c
@@ -28,6 +28,27 @@ static simple_ble_service_t environmental_sensing_service = {{
               0x00,0x10,0x00,0x00,0x1A,0x18,0x00,0x00}
 }};
 
+static simple_ble_char_t temperature_state_char = {.uuid16 = 0x2a6e};
+static int16_t temperature_val = 900;
+
+static simple_ble_char_t sensor_location_state_char = {.uuid16 = 0x2a5d};
+static uint8_t sensor_location_val = 0x02;
+
+static simple_ble_char_t rest_hr_state_char = {.uuid16 = 0x2a92};
+static uint8_t rest_hr_val = 58;
+
+static simple_ble_char_t lang_state_char = {.uuid16 = 0x2aa2};
+static char lang_val[3] = "en";
+
+static simple_ble_char_t floor_no_state_char = {.uuid16 = 0x2ab2};
+static int16_t floor_no_val = 900;
+
+static simple_ble_char_t blood_pressure_state_char = {.uuid16 = 0x1810};
+static uint8_t blood_pressure_val = 69;
+
+static simple_ble_char_t max_hr_state_char = {.uuid16 = 0x2a8d};
+static uint8_t max_hr_val = 69;
+
 /*******************************************************************************
  *   State for this application
  ******************************************************************************/
@@ -46,6 +67,24 @@ int main(void) {
   simple_ble_app = simple_ble_init(&ble_config);
 
   simple_ble_add_service(&environmental_sensing_service);
+  simple_ble_add_characteristic(1, 0, 0, 0,
+    sizeof(temperature_val), (uint8_t*)&temperature_val, 
+    &environmental_sensing_service, &temperature_state_char);
+  simple_ble_add_characteristic(1, 0, 0, 0,
+    sizeof(sensor_location_val), (uint8_t*)&sensor_location_val, 
+    &environmental_sensing_service, &sensor_location_state_char);
+  simple_ble_add_characteristic(1, 0, 0, 0,
+    sizeof(rest_hr_val), (uint8_t*)&rest_hr_val, 
+    &environmental_sensing_service, &rest_hr_state_char);
+  simple_ble_add_characteristic(1, 0, 0, 0,
+    sizeof(max_hr_val), (uint8_t*)&max_hr_val, 
+    &environmental_sensing_service, &max_hr_state_char);
+  simple_ble_add_characteristic(1, 0, 0, 0,
+    sizeof(lang_val), (uint8_t*)&lang_val, 
+    &environmental_sensing_service, &lang_state_char);
+  simple_ble_add_characteristic(1, 0, 0, 0,
+    sizeof(blood_pressure_val), (uint8_t*)&blood_pressure_val, 
+    &environmental_sensing_service, &blood_pressure_state_char);
 
   // Start Advertising
   simple_ble_adv_only_name();
